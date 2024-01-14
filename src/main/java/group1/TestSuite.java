55bd85bba60bf619c90f17a51c180906670959d8
AliaAzam12 <alianabilah3031@gmail.com>
2024-01-14 18:12:25 +0800

    software testing

---
+++ src/main/java/group1/TestSuite.java
@@ -0,0 +1,19 @@
+package group1;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        Alia_287784.class,
+})
+
+public class TestSuite {
+
+}
+
+//reports
+//run - command prompt - ANT/Maven
+//EXE file
+//Jenkins - ANT/Maven
