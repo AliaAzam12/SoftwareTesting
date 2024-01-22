c2d70cf7711624507f5c3c513aff1dd4e3267272
08bai15 <94061603+08bai15@users.noreply.github.com>
2024-01-22 22:44:48 +0800

    TestSuite.java

--- src/main/java/group1/TestSuite.java
+++ src/main/java/group1/TestSuite.java
@@ -6,8 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        Nissaa281866.class,
         Alia_287784.class,
+        Nissaa281866.class,
         abdulrahman_274922.class,
         Chan_286596.class,
         Farhana_284586.class,
